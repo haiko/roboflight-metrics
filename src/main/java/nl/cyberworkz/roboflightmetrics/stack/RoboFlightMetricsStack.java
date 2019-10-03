05bea772906ac203f14877732cdc2dd8328fb53e
Haiko <hvdschaaf@gmail.com>
2019-10-03 22:48:41 +0200

    add dynamodb table

--- src/main/java/nl/cyberworkz/roboflightmetrics/stack/RoboFlightMetricsStack.java
+++ src/main/java/nl/cyberworkz/roboflightmetrics/stack/RoboFlightMetricsStack.java
@@ -3,6 +3,8 @@ package nl.cyberworkz.roboflightmetrics.stack;
 import software.amazon.awscdk.core.Construct;
 import software.amazon.awscdk.core.Duration;
 import software.amazon.awscdk.core.Stack;
+import software.amazon.awscdk.services.dynamodb.Table;
+import software.amazon.awscdk.services.dynamodb.TableProps;
 import software.amazon.awscdk.services.lambda.Runtime;
 import software.amazon.awscdk.services.lambda.SingletonFunction;
 import software.amazon.awscdk.services.lambda.SingletonFunctionProps;
@@ -12,6 +14,16 @@ public class RoboFlightMetricsStack extends Stack {
     RoboFlightMetricsStack(final Construct parent, final String name) {
         super(parent, name);
 
+        //DynamoDB
+        Table metricsTable = new Table(this, "metricsTable", TableProps.builder()
+                .withTableName("metrics")
+                .withReadCapacity(1)
+                .withWriteCapacity(1)
+                .withPartitionKey()
+                .build());
+
+
+        // Lambda
         SingletonFunction metricsFunction = new SingletonFunction(this, "cdk-lambda-metrics",
                 SingletonFunctionProps.builder()
                         .withDescription("Collection metrics from AWS SQS queue")
