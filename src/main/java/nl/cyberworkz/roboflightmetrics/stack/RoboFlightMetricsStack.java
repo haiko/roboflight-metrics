6376f51bef205d318c3491e57eb150bb7ff21fa0
Haiko <hvdschaaf@gmail.com>
2019-10-04 15:17:10 +0200

    add SQS queue and existing lambda function

--- src/main/java/nl/cyberworkz/roboflightmetrics/stack/RoboFlightMetricsStack.java
+++ src/main/java/nl/cyberworkz/roboflightmetrics/stack/RoboFlightMetricsStack.java
@@ -3,25 +3,45 @@ package nl.cyberworkz.roboflightmetrics.stack;
 import software.amazon.awscdk.core.Construct;
 import software.amazon.awscdk.core.Duration;
 import software.amazon.awscdk.core.Stack;
+import software.amazon.awscdk.services.dynamodb.Attribute;
+import software.amazon.awscdk.services.dynamodb.AttributeType;
 import software.amazon.awscdk.services.dynamodb.Table;
 import software.amazon.awscdk.services.dynamodb.TableProps;
+import software.amazon.awscdk.services.events.targets.LambdaFunction;
+import software.amazon.awscdk.services.events.targets.SqsQueue;
+import software.amazon.awscdk.services.lambda.*;
 import software.amazon.awscdk.services.lambda.Runtime;
-import software.amazon.awscdk.services.lambda.SingletonFunction;
-import software.amazon.awscdk.services.lambda.SingletonFunctionProps;
+import software.amazon.awscdk.services.sqs.Queue;
+import software.amazon.awscdk.services.sqs.QueueProps;
 
 public class RoboFlightMetricsStack extends Stack {
 
     RoboFlightMetricsStack(final Construct parent, final String name) {
         super(parent, name);
 
+        // RoboFlightMonitor Lambda - existing resource in same account
+        IFunction flightMonitorFn = Function.fromFunctionArn(this, "RoboFlightMonitorAPI", "arn:aws:lambda:eu-central-1:184690513648:function:awscodestar-roboflightmonit-l-RoboFlightMonitorAPI-AUN6CSV9YOJT");
+
+
+        // SQS Queue
+        Queue monitorEventQueue  = new Queue(this, "metrics-queue", QueueProps.builder()
+                .withQueueName("metrics-queue")
+                .build());
+
         //DynamoDB
         Table metricsTable = new Table(this, "metricsTable", TableProps.builder()
                 .withTableName("metrics")
                 .withReadCapacity(1)
                 .withWriteCapacity(1)
-                .withPartitionKey()
+                .withPartitionKey(Attribute.builder()
+                        .withName("event_time")
+                        .withType(AttributeType.STRING)
+                        .build())
+                .withSortKey(Attribute.builder()
+                        .withName("origin_flight")
+                        .withType(AttributeType.STRING)
+                        .build())
                 .build());
-
 
         // Lambda
         SingletonFunction metricsFunction = new SingletonFunction(this, "cdk-lambda-metrics",
@@ -32,5 +52,10 @@ public class RoboFlightMetricsStack extends Stack {
                         .withTimeout(Duration.seconds(60))
                         .withMemorySize(256).build());
 
+        //grant sending messages and consuming messages to the queue .
+        monitorEventQueue.grantSendMessages(flightMonitorFn);
+        monitorEventQueue.grantConsumeMessages(metricsFunction);
+
+        
     }
 }
